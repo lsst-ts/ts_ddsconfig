c3289d488203fd1866bab2ef2d92135420b91f11
Russell Owen <rowen@uw.edu>
2021-03-30 11:55:22 -0700

    doc/conf.py: add a missing `# noqa`

--- doc/conf.py
+++ doc/conf.py
@@ -11,4 +11,4 @@ html_theme_options["logotext"] = project  # noqa
 html_title = project
 html_short_title = project
 doxylink = {}  # Avoid warning: Could not find tag file _doxygen/doxygen.tag
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # noqa
